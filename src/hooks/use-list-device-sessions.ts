9cedbe5b0b8e11e913ac424a3c79a2ce4068c3f0
daveycodez <david@swiftapk.com>
2025-02-09 20:28:08 -0800

    Reset entire QueryClient on setActiveSession

--- src/hooks/use-list-device-sessions.ts
+++ src/hooks/use-list-device-sessions.ts
@@ -124,18 +124,19 @@ export function useListDeviceSessions<
             }
         },
         onSettled: (data, error, sessionToken, context) => {
-            const previousData = queryClient.getQueryData(queryKey) as SessionData[] | undefined
-            const newSession = previousData?.find((sessionData) => sessionData.session.token === sessionToken)
+            queryClient.resetQueries()
+            // const previousData = queryClient.getQueryData(queryKey) as SessionData[] | undefined
+            // const newSession = previousData?.find((sessionData) => sessionData.session.token === sessionToken)
 
-            if (newSession) {
-                queryClient.setQueryData(sessionKey, () => newSession)
-            }
+            // if (newSession) {
+            //     queryClient.setQueryData(sessionKey, () => newSession)
+            // }
 
-            queryClient.invalidateQueries({ queryKey: sessionKey })
-            queryClient.invalidateQueries({ queryKey: tokenKey })
-            queryClient.resetQueries({ queryKey: listAccountsKey })
-            queryClient.resetQueries({ queryKey: listSessionsKey })
-            queryClient.invalidateQueries({ queryKey })
+            // queryClient.invalidateQueries({ queryKey: sessionKey })
+            // queryClient.invalidateQueries({ queryKey: tokenKey })
+            // queryClient.resetQueries({ queryKey: listAccountsKey })
+            // queryClient.resetQueries({ queryKey: listSessionsKey })
+            // queryClient.invalidateQueries({ queryKey })
         }
     })
 
