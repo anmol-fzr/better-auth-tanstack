176d630defe3574b0e1e354ef9963434892c273e
daveycodez <david@swiftapk.com>
2025-02-09 20:21:22 -0800

    Slightly more optimistic setActiveSession

--- src/hooks/use-list-device-sessions.ts
+++ src/hooks/use-list-device-sessions.ts
@@ -123,11 +123,18 @@ export function useListDeviceSessions<
                 )
             }
         },
-        onSettled: () => {
+        onSettled: (data, error, sessionToken, context) => {
+            const previousData = queryClient.getQueryData(queryKey) as SessionData[] | undefined
+            const newSession = previousData?.find((sessionData) => sessionData.session.token === sessionToken)
+
+            if (newSession) {
+                queryClient.setQueryData(sessionKey, () => newSession)
+            }
+
             queryClient.invalidateQueries({ queryKey: sessionKey })
-            queryClient.invalidateQueries({ queryKey: tokenKey })
-            queryClient.invalidateQueries({ queryKey: listAccountsKey })
-            queryClient.invalidateQueries({ queryKey: listSessionsKey })
+            queryClient.resetQueries({ queryKey: tokenKey })
+            queryClient.resetQueries({ queryKey: listAccountsKey })
+            queryClient.resetQueries({ queryKey: listSessionsKey })
             queryClient.invalidateQueries({ queryKey })
         }
     })
